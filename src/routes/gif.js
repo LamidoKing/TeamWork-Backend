0a97a4527a947b03c9bf85cb7833812ec3a8bacb
lamido <kinglamido@gmail.com>
2019-11-12 15:12:09 +0100

    ft(delete-gif):add user-can-delete-gif [finishes #7]

--- src/routes/gif.js
+++ src/routes/gif.js
@@ -1,9 +1,10 @@
 const Router = require('express-promise-router');
-const { postGif } = require('../controllers');
+const { postGif, deleteGif } = require('../controllers');
 const { auth, multerUploads } = require('../middleware');
 const { cloudinary } = require('../config');
 
 const router = new Router();
 
 router.post('/gifs', auth, cloudinary, multerUploads, postGif);
+router.delete('/gifs/:id', auth, deleteGif);
 module.exports = router;
